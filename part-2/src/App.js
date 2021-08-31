cb21fb1b706b62c76c93b8dd445f55b95fb26874
Shawn Schneider <shawns2759@gmail.com>
2021-08-31 10:58:18 -0700

    fso forms aug 31

--- part-2/src/App.js
+++ part-2/src/App.js
@@ -8,13 +8,13 @@ const GreatestNum = ({nums}) => {
   for (let n in nums) {
     total += nums[n]
   }
-  console.log(total + 'total');
+  // console.log(total + 'total');
 
 
   let evenNums = nums.filter((num) => {
     return num % 2 === 0 
   })
-  console.log(evenNums);
+  // console.log(evenNums);
   
   function showNums(set) {
     let nums = set.map((num) => {
@@ -41,7 +41,7 @@ const App = ({notes, nums}) => {
   return (
     <div>
       <h1>Notes</h1>
-      <GreatestNum nums={nums} />
+      {/* <GreatestNum nums={nums} /> */}
       <Notes notes={notes}/>
     </div>
   )
--- part-2/src/components/Note.js
+++ part-2/src/components/Note.js
@@ -1,31 +1,106 @@
 import { React, useState } from 'react';
 
 
-const Notes = ({notes}) => {
-  let {note, setNote} = useState([])
-  const [newNote, setNewNote] = useState('note1')
+const Notes = ({ notes }) => {
+
+  const [note, setNote] = useState(notes)
+  const [newNote, setNewNote] = useState('')
+
     let allNotes = notes.map((note,i) => {
       return <li key={note.id}>{note.id}: {note.content}, {i}</li>
     })
   
-  function addNote(event) {
-    setNewNote(event.target.value)
+  const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNote(notes.concat(noteObject))
+    setNewNote('')
+    console.log(newNote);
 }
+  
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
     return (
       <div>
         <ul>
           {allNotes}
         </ul>
-        {newNote}
-        <form  onSubmit={addNote}>
+        {newNote} 
+        <form onSubmit={addNote} action='#'>
           <label for="note">Add New Note</label>
-          <input type="text" id="note" />
-          <button>Submit</button>
+          <input type="text" onChange={handleNoteChange} value={newNote}/>
+          <button type='submit'>Submit</button>
         </form>
       </div>
     )
 }
-  
+
 export default Notes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Form = () => {
+//   const [username, setUserName] = useState('')
+//   const isLower = username === username.toLowerCase()
+//   const error = isLower ? null : 'username must be lowercase';
+//   const handleChange = (e) => {
+//     setUserName(e.target.value.toLowerCase())
+//   }
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+//     console.log(username);
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="note">Note</label>
+//         <input id='note' name='note' type="text" onChange={handleChange} value={username}/>
+//         <div style={{ color: 'red' }}>
+//         {error}
+//         </div>
+//         <button disabled={Boolean(error)}>Submit</button>
+//       </form>
+//       {username}
+//     </div>
+//   )
+// }
+
+// const Notes = ({notes}) => {
+//   // let {newNote, setNewNote} = useState('old note')
+//     let note = notes.map((note) => {
+//       return <li key={note.id}>{note.content}</li>
+//     })
+  
+  
+//   return (
+//     < div >
+//       {note}
+//     <Form/>
+//     </div >
+//   )
+// }
+  
