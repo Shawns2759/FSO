ffb19d249d2891bc1fb120e3d642fbdcafc018b6
Shawn Schneider <shawns2759@gmail.com>
2021-09-02 16:29:11 -0700

    refactored code

--- phonebook/src/AddContact.jsx
+++ phonebook/src/AddContact.jsx
@@ -1,19 +1,109 @@
 import { useState, React } from 'react'
 
 
-const AddContact = ({ handleSubmit, handleChange, inputVal, handleNumberChange, numVal}) => {
-    return (
-      <form onSubmit={handleSubmit}>
-          <div>
-            name:<input onChange={handleChange} value={inputVal} name="personName" />
-            number:<input type="number" onChange={handleNumberChange} value={numVal} name="personNumber" />
+// const AddContact = ({ handleSubmit, handleChange, inputVal, handleNumberChange, numVal}) => {
+//     return (
+//       <form onSubmit={handleSubmit}>
+//           <div>
+//             name:<input onChange={handleChange} value={inputVal} name="personName" />
+//             number:<input type="number" onChange={handleNumberChange} value={numVal} name="personNumber" />
   
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-    </form>
+//           </div>
+//           <div>
+//             <button type="submit">Submit</button>
+//           </div>
+//     </form>
+//     )
+// }
+  
+const AddContact = ({ newName, person, inputVal, numVal,setNewName, setPerson, setInputVal, setNumVal }) => {
+    
+    const handleChange = (e) => {
+        setInputVal(e.target.value)
+      }
+    
+      const handleNumberChange = (e) => {
+        setNumVal(e.target.value)
+      }
+    
+      function isRepeat(inputVal) {
+        // console.log(inputVal, person[0].name);
+        let result = []
+        for (let i in person) {
+          if (person[i].name !== inputVal) {
+            continue
+          } else {
+            result.push(person[i].name)
+          }
+        }
+        if (result.length) {
+          return {
+            bool: false,
+            result: result
+          }
+        } else {
+          return {
+            bool: true,
+            result: null
+          }
+        }
+      }
+      const persons = () => {
+        let p = person.map((i) => {
+          return <li key={i.id}>{i.name}   {i.num}</li>
+        })
+       return p 
+      }
+     
+    
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        setNewName(inputVal)
+        // console.log(person)
+        let x = person.length + 1
+        let nameObj = {
+          name: inputVal,
+          num: numVal,
+          id: x
+        }
+        // console.log(Boolean(isRepeat(inputVal)));
+        let duplicated = isRepeat(inputVal)
+    
+        if (duplicated.bool === false) {
+          alert(`${duplicated.result} already used`)
+          return 
+        }
+        let p = person.concat(nameObj)
+        // console.log(p);
+        setPerson(p)
+        setInputVal('')
+        setNumVal('')
+      }
+
+
+
+    return (
+        <div>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                  name:<input onChange={handleChange} value={inputVal} name="personName" />
+                  number:<input type="number" onChange={handleNumberChange} value={numVal} name="personNumber" />
+        
+                </div>
+                <div>
+                  <button type="submit">Submit</button>
+                  </div>
+          </form>
+            <div>
+                <ul>
+                    {persons()}
+                </ul>
+            </div>
+        </div>
     )
 }
-  
+
+
+
 export default AddContact
--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -9,6 +9,7 @@ import AddContact from './AddContact'
 
 
 function App() {
+
   const [newName, setNewName] = useState('')
   const [person, setPerson] = useState([
     { name: 'shawn',num: '208-123-4567' ,id: 1 },
@@ -18,83 +19,15 @@ function App() {
   const [inputVal, setInputVal] = useState('')
   const [numVal, setNumVal] = useState('')
 
-  
 
-  const handleChange = (e) => {
-    setInputVal(e.target.value)
-  }
 
-  const handleNumberChange = (e) => {
-    setNumVal(e.target.value)
-  }
-
-  function isRepeat(inputVal) {
-    // console.log(inputVal, person[0].name);
-    let result = []
-    for (let i in person) {
-      if (person[i].name !== inputVal) {
-        continue
-      } else {
-        result.push(person[i].name)
-      }
-    }
-    if (result.length) {
-      return {
-        bool: false,
-        result: result
-      }
-    } else {
-      return {
-        bool: true,
-        result: null
-      }
-    }
-  }
-  const persons = () => {
-    let p = person.map((i) => {
-      return <li key={i.id}>{i.name}   {i.num}</li>
-    })
-   return p 
-  }
- 
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setNewName(inputVal)
-    // console.log(person)
-    let x = person.length + 1
-    let nameObj = {
-      name: inputVal,
-      num: numVal,
-      id: x
-    }
-    // console.log(Boolean(isRepeat(inputVal)));
-    let duplicated = isRepeat(inputVal)
-
-    if (duplicated.bool === false) {
-      alert(`${duplicated.result} already used`)
-      return 
-    }
-    let p = person.concat(nameObj)
-    // console.log(p);
-    setPerson(p)
-    setInputVal('')
-    setNumVal('')
-  }
   return (
     <div>
       <div>debug: {newName}</div>
-
-      
-   
       <h2>PhoneBook</h2>
-
       <Search person={person}/>
-      <AddContact handleSubmit={handleSubmit} handleChange={handleChange} inputVal={inputVal} handleNumberChange={handleNumberChange} numVal={numVal}/>
+      <AddContact  newName={newName} person={person} inputVal={inputVal} numVal={numVal} setNewName={setNewName} setPerson={setPerson} setInputVal={setInputVal} setNumVal={setNumVal}/>
       <div>
-        <ul>
-          {persons()}
-        </ul>
       </div>
     </div>
   );
